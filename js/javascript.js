3f3191af379351365fcaed7e4bd5b19d89859b86
abelsanxd <abelsan4665@gmail.com>
2023-12-20 19:05:30 -0300

    se realizo una variable con el nombre completo y otra que calcula los grados celsius

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
-let impar = 33;
-let par = 50;
+let impar = 23;
+let par = 60;
 
 document.write("El resultado de la suma es: " + (impar + par));
 
@@ -8,3 +8,21 @@ document.write("<br>El resultado de la resta es: " + (impar - par));
 document.write("<br>El resultado de la multiplicacion es: " + impar * par);
 
 document.write("<br>El resultado de la division es: " + impar / par);
+
+let nombre = "Abel";
+let apellido = " Perez";
+
+let nombreCompleto = nombre + apellido;
+
+document.write("<br>El nombre completo es: " + nombreCompleto);
+
+gradosCelsius = 25;
+gradosFahrenheit = (gradosCelsius * 9) / 5 + 32;
+
+document.write(
+  "<br>" +
+    gradosCelsius +
+    "°C grados celsius son equivalentes a " +
+    gradosFahrenheit +
+    "°F grados farenheit"
+);
