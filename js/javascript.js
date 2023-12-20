b78a7e0e9f7e78ebfbc0ca736bb3b185b020ce7e
abelsanxd <abelsan4665@gmail.com>
2023-12-20 19:02:45 -0300

    se realizo suma, resta, multiplicacion y division con variables

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+let impar = 33;
+let par = 50;
+
+document.write("El resultado de la suma es: " + (impar + par));
+
+document.write("<br>El resultado de la resta es: " + (impar - par));
+
+document.write("<br>El resultado de la multiplicacion es: " + impar * par);
+
+document.write("<br>El resultado de la division es: " + impar / par);
