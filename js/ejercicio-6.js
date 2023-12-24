ea9d246fec7955da1c362f9beea4ec8f3226827a
abelsanxd <abelsan4665@gmail.com>
2023-12-24 01:25:51 -0300

    se creo un programa que te dice que numero es mayor

---
+++ js/ejercicio-6.js
@@ -0,0 +1,8 @@
+let numero1 = parseFloat(prompt("Ingrese el primer número: "));
+let numero2 = parseFloat(prompt("Ingrese el segundo número: "));
+
+if (numero1 > numero2) {
+  alert(numero1 + " es mayor");
+} else {
+  alert(numero2 + " es mayor");
+}
